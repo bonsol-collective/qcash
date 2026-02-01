b172926115d88a6c933ca45fce7846c6e0862a6e
Chris Tam <chris-tam8@hotmail.com>
2026-02-02 02:51:07 +0800

    Add BSL license headers to key files

--- methods/src/lib.rs
+++ methods/src/lib.rs
@@ -1,2 +1,9 @@
+// Business Source License 1.1 (BSL 1.1)
+// Licensor: Bonsol Labs
+// Licensed Work: QCash
+// Change Date: 2030-12-31
+// Change License: Apache License 2.0
+// Use of this software is governed by the LICENSE file.
+
 // Include the generated guest program ELF
 include!(concat!(env!("OUT_DIR"), "/methods.rs"));
--- qcash-faucet/backend-ts/src/index.ts
+++ qcash-faucet/backend-ts/src/index.ts
@@ -1,3 +1,10 @@
+// Business Source License 1.1 (BSL 1.1)
+// Licensor: Bonsol Labs
+// Licensed Work: QCash
+// Change Date: 2030-12-31
+// Change License: Apache License 2.0
+// Use of this software is governed by the LICENSE file.
+
 import express from "express";
 import cors from "cors";
 import * as anchor from "@coral-xyz/anchor";
