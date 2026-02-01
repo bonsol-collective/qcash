65fec6ce3e5f99045a061823c041941ec34cdaba
Chris Tam <chris-tam8@hotmail.com>
2026-02-02 02:59:19 +0800

    Update licensor name to Bonsol Labs Inc.

--- daemon_wrapper.sh
+++ daemon_wrapper.sh
@@ -1,5 +1,5 @@
 # Business Source License 1.1 (BSL 1.1)
-# Licensor: Bonsol Labs
+# Licensor: Bonsol Labs Inc.
 # Licensed Work: QCash
 # Change Date: 2030-12-31
 # Change License: Apache License 2.0
